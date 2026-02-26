4dc564a9037e007f0b6c9ec67f609750f8f1e987
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-25 22:13:01 -0300

    Projeto Finaizado

--- src/main/java/br/fiap/com/ms/produto/controller/ProdutoController.java
+++ src/main/java/br/fiap/com/ms/produto/controller/ProdutoController.java
@@ -4,11 +4,10 @@ import br.fiap.com.ms.produto.Services.ProdutoService;
 import br.fiap.com.ms.produto.dto.ProdutoDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController // definiçâo
@@ -25,4 +24,43 @@ public class ProdutoController {
 
         return ResponseEntity.ok(list);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProdutoDTO> getProdutosById(@PathVariable Long id){
+
+        ProdutoDTO  produtoDTO = produtoService.findProdutoById(id);
+
+        return ResponseEntity.ok(produtoDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<ProdutoDTO> createProduto(@RequestBody ProdutoDTO produtoDTO){
+
+        produtoDTO = produtoService.saveProduto(produtoDTO);
+
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequestUri()
+                .path("/{id}")
+                .buildAndExpand(produtoDTO.getId())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(produtoDTO);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProdutoDTO> updateProduto(@PathVariable Long id,
+                                                    @RequestBody ProdutoDTO produtoDTO){
+        produtoDTO = produtoService.updateProduto(id, produtoDTO);
+
+        return ResponseEntity.ok(produtoDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduto(@PathVariable Long id){
+
+        produtoService.deleteProdutoById(id);
+
+        return ResponseEntity.noContent().build();
+    }
+
 }
